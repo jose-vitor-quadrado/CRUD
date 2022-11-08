f75354e004938529c4da9f4625c376bbc1eaa4ec
Jose Vitor Quadrado <josevitorquadrado@gmail.com>
2022-11-08 15:38:58 -0300

    created project and database

---
+++ Models/Post.cs
@@ -0,0 +1,17 @@
+namespace Blog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Summary { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+        public string Slug { get; set; } = string.Empty;
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+        public Category Category { get; set; } = null!;
+        public User Author { get; set; } = null!;
+
+        public List<Tag> Tags { get; set; } = null!;
+    }
+}
