9f01cd415c8255ef67cb1d136cbb7021b20f23e5
Jose Vitor Quadrado <josevitorquadrado@gmail.com>
2022-11-09 14:00:53 -0300

    added view model EditorCategoryViewModel

---
+++ ViewModel/EditorCategoryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Blog.ViewModel
+{
+    public class EditorCategoryViewModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Slug { get; set; } = string.Empty;
+    }
+}
