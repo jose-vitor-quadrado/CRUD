af74923d4d2c5348d2eeee32424afab6eeb13b8a
Jose Vitor Quadrado <josevitorquadrado@gmail.com>
2022-11-10 21:38:46 -0300

    generating jwt token

---
+++ Configuration.cs
@@ -0,0 +1,6 @@
+﻿namespace Blog;
+
+public static class Configuration
+{
+    public static string JwtKey { get; set; } = "MWFhZTYyYmItODAyOS00M2M1LWJlZmMtZTU1ZjNkNDgyMThk";
+}
---
+++ Services/TokenService.cs
@@ -0,0 +1,31 @@
+﻿using Blog.Models;
+using System.Text;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Blog.Services;
+
+public class TokenService
+{
+    public string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new (ClaimTypes.Name, "andrebaltieri"),
+                new (ClaimTypes.Role, "admin"),
+                new ("hero", "ashe")
+            }),
+            Expires = DateTime.UtcNow.AddHours(8),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key), 
+                SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
