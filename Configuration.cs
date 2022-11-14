6de5f0bf19d021fb41def273edfe3973a893b3e3
Jose Vitor Quadrado <josevitorquadrado@gmail.com>
2022-11-14 13:37:49 -0300

    Reading from AppSettings

--- Configuration.cs
+++ Configuration.cs
@@ -5,4 +5,13 @@ public static class Configuration
     public static string JwtKey = "MWFhZTYyYmItODAyOS00M2M1LWJlZmMtZTU1ZjNkNDgyMThk";
     public static string ApiKeyName = "api_key";
     public static string ApiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";
+    public static SmtpConfiguration Smtp = new();
+
+    public class SmtpConfiguration
+    {
+        public string Host { get; set; } = string.Empty;
+        public int Post { get; set; } = 25;
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
 }
